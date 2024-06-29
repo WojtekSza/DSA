dd557cd5b2fe00c73a4c64ea066c74e78fd060f5
WojtekSza <86256349+WojtekSza@users.noreply.github.com>
2024-06-29 07:08:17 +0200

    Time: 46 ms (76.47%) | Memory: 16.7 MB (43.57%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -7,11 +7,15 @@ class Solution:
             while j<len(prices):
                 if prices[i]-prices[j]>=0:
                         stack.append(prices[i]-prices[j])
+                        break
                 j+=1
             if stack:
                 ans.append(stack[0])
             else:
                 ans.append(prices[i])
         return ans
+
+            
+        
 
         
